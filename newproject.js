4edefac04a7f36259d2f3310a68187f2e8fffae1
szoker <szoker527@gmail.com>
2021-08-04 16:20:47 +0200

    added game function and made basic loop

--- newproject.js
+++ newproject.js
@@ -11,55 +11,87 @@ function computerPlay() {
     }
     else {
         return "scissors"
-    };
-    "sdasd"
+    }
+    
 }   
+function game(){
+    let score
+
+    let playerSelection = prompt("Rock?  Paper?  Scissors?");
+     computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+
+    playerSelection = prompt("Rock?  Paper?  Scissors?");
+     computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+    
+    playerSelection = prompt("Rock?  Paper?  Scissors?");
+     computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+    
+    playerSelection = prompt("Rock?  Paper?  Scissors?");
+     computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+
+    playerSelection = prompt("Rock?  Paper?  Scissors?");
+     computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+    }
+
+function game1() {
+    playerSelection = prompt("Rock?  Paper?  Scissors?");
+    let score 
+    score = playRound(playerSelection, computerSelection);
+
+    if (score == "You lost") {
+        alert("0:1")
+    }
+    else if (score == "You Won") {
+        alert("1:0")
+    }
+    else if (score == "Draw") {
+        alert("0:0")
+    }
+    else {
+        alert("something wrong")
+    }
+}
+
 
     function playRound(playerSelection, computerSelection) {
           
             if (playerSelection.toLowerCase() == "rock" && computerSelection == "rock") {
-                return alert("Draw")
+                 alert("Draw")
             }
             else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
-                return alert("You lost")
+                 alert("You lost")
             }
             else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
-                return alert("You Won")
+                 alert("You Won")
             }
             else if (playerSelection.toLowerCase() == "paper" && computerSelection == "paper") {
-                return alert("Draw")
+                 alert("Draw")
             }
             else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
-                return alert("You lost")
+                 alert("You lost")
             }
             else if (playerSelection.toLowerCase() == "paper" &&  computerSelection == "rock") {
-                return alert("You Won")
+                 alert("You Won")
             }
             else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "scissors") {
-                return alert("Draw")
+                 alert("Draw")
             }
             else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
-                return alert("You lost")
+                 alert("You lost")
             }
             else if (playerSelection.toLowerCase() == "scissors" &&  computerSelection == "paper") {
-                return alert("You Won")
+                 alert("You Won")
             }
             else {
-                return alert("something wrong")
+                 alert("something wrong")
             }
 
     }
 
-    function game(){
-        let playerSelection = prompt("Rock?  Paper?  Scissors?")
-        
-        return playRound(playerSelection, computerSelection)
 
-    }
-
-    
-
-const computerSelection = computerPlay();
-
-
-console.log(playRound(playerSelection, computerSelection));
+    let computerSelection = computerPlay();
